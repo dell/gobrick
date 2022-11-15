b0c21caa6e9a77a9b0b4310ac513d9c94ba41b38
delldubey <Utkarsh.Dubey@dell.com>
2022-11-15 16:53:27 +0530

    Adds connectRDM for powermax (#28)

---
+++ rdm.go
@@ -0,0 +1,118 @@
+package gobrick
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/dell/gobrick/internal/logger"
+	"github.com/dell/gobrick/internal/tracer"
+	"github.com/dell/gobrick/pkg/scsi"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"strconv"
+)
+
+// RDMVolumeInfo has request info for RDM device
+type RDMVolumeInfo struct {
+	Targets []FCTargetInfo
+	Lun     int
+	WWN     string
+}
+
+// ConnectRDMVolume attach RDM to a VM
+func (fc *FCConnector) ConnectRDMVolume(ctx context.Context, info RDMVolumeInfo) (Device, error) {
+	defer tracer.TraceFuncCall(ctx, "FCConnector.ConnectVsphereVolume")()
+	if err := fc.limiter.Acquire(ctx, 1); err != nil {
+		return Device{}, errors.New("too many parallel operations. try later")
+	}
+	defer fc.limiter.Release(1)
+	if err := fc.validateRDMVolumeInfo(ctx, info); err != nil {
+		return Device{}, err
+	}
+	device, err := fc.connectRDM(ctx, info)
+	if err == nil {
+		return device, nil
+	}
+	logger.Error(ctx, "failed to connect RDM volume: %s, try to cleanup", err.Error())
+	_ = fc.cleanRDMConnection(ctx, true, info)
+	return Device{}, err
+}
+
+func (fc *FCConnector) cleanRDMConnection(ctx context.Context, force bool, info RDMVolumeInfo) error {
+	defer tracer.TraceFuncCall(ctx, "FCConnector.cleanRDMConnection")()
+	wwn := fmt.Sprintf("3%s", info.WWN)
+	devices, err := fc.scsi.GetDevicesByWWN(ctx, wwn)
+	if err != nil || len(devices) == 0 {
+		msg := "failed to get devices by WWN: " + wwn
+		logger.Error(ctx, msg)
+		return errors.New(msg)
+	}
+	logger.Info(ctx, "devices found: %s", devices)
+	return fc.baseConnector.cleanDevices(ctx, force, devices)
+}
+
+func (fc *FCConnector) validateRDMVolumeInfo(ctx context.Context, info RDMVolumeInfo) error {
+	defer tracer.TraceFuncCall(ctx, "FCConnector.validateRDMVolumeInfo")()
+	if len(info.Targets) == 0 {
+		return errors.New("at least one FC target required")
+	}
+	if info.WWN == "" {
+		return errors.New("wwn of RDM required")
+	}
+	return nil
+}
+
+func (fc *FCConnector) connectRDM(ctx context.Context, info RDMVolumeInfo) (Device, error) {
+	defer tracer.TraceFuncCall(ctx, "FCConnector.connectRDM")()
+	err := fc.rescanAllHosts(ctx)
+	if err != nil {
+		return Device{}, err
+	}
+	wwn := fmt.Sprintf("3%s", info.WWN)
+	devices, err := fc.scsi.GetDevicesByWWN(ctx, wwn)
+	if err != nil || len(devices) == 0 {
+		msg := "failed to get devices by WWN: " + wwn
+		logger.Error(ctx, msg)
+		return Device{}, errors.New(msg)
+	}
+	var device string
+	device, err = fc.waitSingleDevice(ctx, wwn, devices)
+	if err != nil {
+		return Device{}, err
+	}
+
+	if !fc.scsi.CheckDeviceIsValid(ctx, path.Join("/dev/", device)) {
+		msg := "device invalid"
+		logger.Error(ctx, msg)
+		return Device{}, errors.New(msg)
+	}
+	d := Device{WWN: wwn, Name: device}
+	return d, nil
+}
+
+func (fc *FCConnector) rescanAllHosts(ctx context.Context) error {
+	defer tracer.TraceFuncCall(ctx, "FCConnector.rescanAllHosts")()
+	hostsDir := "/sys/class/scsi_host"
+	hostFiles, err := os.ReadDir(fmt.Sprintf("%s/", hostsDir))
+	if err != nil {
+		log.Errorf("rescanSCSIHOSTALL failed to read scsi_host dir, err: %s", err.Error())
+		return err
+	}
+	log.Infof("found (%d) files in hostsDir (%s)", len(hostFiles), hostsDir)
+	scsiHost := scsi.NewSCSI("")
+	for host := 0; host < len(hostFiles); host++ {
+		// at least one target port not found, do full scsi rescan
+		hctl := scsi.HCTL{
+			Host:    strconv.Itoa(host),
+			Lun:     "-",
+			Channel: "-",
+			Target:  "-"}
+		err := scsiHost.RescanSCSIHostByHCTL(ctx, hctl)
+		if err != nil {
+			log.Error(ctx, err.Error())
+			continue
+		}
+	}
+	return nil
+}

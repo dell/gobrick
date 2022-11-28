b8db428d0fb852d9a6fe93509ec21b9293afef44
abhi16394 <aabhi66@gmail.com>
2022-11-28 17:08:34 -0500

    Fix lint issues

--- internal/multipath/interface.go
+++ internal/multipath/interface.go
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+// Package multipath is a generated GoMock package.
 package multipath
 
 import "context"
